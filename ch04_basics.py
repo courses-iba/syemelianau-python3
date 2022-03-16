dc2f9d32ee98a26c024afd74434c129c3dcd7a88
Siarhei Yemelianau <SYemelianau@gomel.iba.by>
2022-03-16 17:53:15 +0300

    python3 - ch04_basics - task1, task2 fixes

--- ch04_basics.py
+++ ch04_basics.py
@@ -3,9 +3,7 @@ import math
 
 # The basics: Practical Task 1
 def get_day_week(day, starting_dotw):
-    week_length = 7
-    result = (starting_dotw + day - 1) % week_length
-    return result if result else week_length
+    return (starting_dotw + day - 2) % 7 + 1
 
 
 print(get_day_week(7, 3))
@@ -13,9 +11,7 @@ print(get_day_week(7, 3))
 
 # The basics: Practical Task 2
 def count_digits(n):
-    if isinstance(n, int) and n > 0:
-        return math.ceil(math.log10(n + .1))
-    return 0
+    return math.ceil(math.log10(n + .1))
 
 
 print(count_digits(123456789))
