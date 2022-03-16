26db4c2f04c9c9d5fac2d8fb0eadefea3ec8fed6
Siarhei Yemelianau <SYemelianau@gomel.iba.by>
2022-03-16 16:12:09 +0300

    python3 - ch04_basics - task1, task2

---
+++ ch04_basics.py
@@ -0,0 +1,21 @@
+import math
+
+
+# The basics: Practical Task 1
+def get_day_week(day, starting_dotw):
+    week_length = 7
+    result = (starting_dotw + day - 1) % week_length
+    return result if result else week_length
+
+
+print(get_day_week(7, 3))
+
+
+# The basics: Practical Task 2
+def count_digits(n):
+    if isinstance(n, int) and n > 0:
+        return int(math.ceil(math.log10(n + .1)))
+    return 0
+
+
+print(count_digits(123456789))
