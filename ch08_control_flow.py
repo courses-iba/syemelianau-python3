031ac5d3fdb3cf2624ce61ab7db3538646123005
Siarhei Yemelianau <SYemelianau@gomel.iba.by>
2022-03-22 20:22:48 +0300

    python3 - ch08_control_flow - task1 - code simplified

--- ch08_control_flow.py
+++ ch08_control_flow.py
@@ -1,23 +1,17 @@
 # Control flow: Practical Task 1
 def transport_hub(schedule, days):
+    result = 0
     capacity = 0
-    max_per_day = []
+    sch = [tuple(map(int, veh.split(' '))) for veh in schedule]
     for day in range(1, days + 1):
-        vehicles = []
-        for vehicle in schedule:
-            per, cap = vehicle.split(' ')
-            if day % int(per) == 0:
-                vehicles.append(int(cap))
-        producer, consumer = 0, 0
-        for vehicle in vehicles:
-            if vehicle > 0:
-                producer += vehicle
-            else:
-                consumer -= vehicle
+        vehicles = [cap for per, cap in sch if day % per == 0]
+        producer = sum([vehicle for vehicle in vehicles if vehicle > 0])
+        consumer = sum([vehicle for vehicle in vehicles if vehicle < 0])
         capacity += producer
-        max_per_day.append(capacity)
-        capacity -= consumer if capacity > consumer else capacity
-    return max(max_per_day)
+        if result < capacity:
+            result = capacity
+        capacity = capacity + consumer if capacity > -consumer else 0
+    return result
 
 
 print(transport_hub(['2 -2', '3 3'], 7))
