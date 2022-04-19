73d621a552b134817be17a2214cc25e52cfeab40
Siarhei Yemelianau <SYemelianau@gomel.iba.by>
2022-04-19 04:01:04 +0300

    python3 - ch15_classes - task1 - saved nonzero elements only & each operation returns a new matrix

--- ch15_classes.py
+++ ch15_classes.py
@@ -1,4 +1,4 @@
-from operator import add, sub, mul
+from operator import add, sub
 from ch09_functions import gen_stream
 
 
@@ -7,48 +7,56 @@ class Matrix:
     def __init__(self, rows, columns, gen):
         self.rows = rows
         self.cols = columns
-        self.data = tuple(tuple(gen_stream(
-            rows * columns,
-            [cell for cell in gen()],
-            self.__extractor
-        ))[i:i + columns] for i in range(0, rows * columns, columns))
+        self.data = tuple(cell for cell in gen())
 
-    def __extractor(self, cell):
-        row, col, val = cell
-        if row >= self.rows or col >= self.cols:
-            raise ValueError
-        return row * self.cols + col, val
+    def __stream(self):
+        return gen_stream(self.rows * self.cols, self.data, lambda cell: (cell[0] * self.cols + cell[1], cell[2]))
+
+    def __matrix(self):
+        return zip(*[iter(self.__stream())] * self.cols)
 
     def __operator(self, other, operation):
-        return tuple(tuple(map(operation, self.data[i], other[i])) for i in range(len(self.data)))
-
-    def __operate(self, other, operation):
-        rows = len(other)
-        cols = len(other[0]) if rows else 0
-        if self.rows != rows and self.cols != cols:
+        if self.rows != other.rows and self.cols != other.cols:
             raise ValueError
-        return self.__operator(other, operation)
+        return Matrix(
+            self.rows,
+            self.cols,
+            lambda: tuple(
+                (int(index / self.cols), int(index % self.cols), cell)
+                for index, cell in enumerate(map(operation, self.__stream(), other.__stream())) if cell
+            )
+        )
 
     def mult(self, other):
-        rows = len(other)
-        if self.cols != rows:
+        if self.cols != other.rows:
             raise ValueError
-        return tuple(tuple(sum(a * b for a, b in zip(row, col)) for col in zip(*other)) for row in self.data)
+        data = tuple()
+        for i, row in enumerate(
+                tuple(tuple(
+                    sum(a * b for a, b in zip(row, col)) for col in zip(*other.__matrix())
+                ) for row in self.__matrix())
+        ):
+            data += tuple((i, j, val) for j, val in enumerate(row) if val != 0)
+        return Matrix(self.rows, other.cols, lambda: data)
 
     def mult_scalar(self, scalar):
-        return self.__operator(tuple((scalar,) * len(row) for row in self.data), mul)
+        return Matrix(self.rows, self.cols, lambda: tuple((row, col, val * scalar) for row, col, val in self.data))
 
     def sum(self, other):
-        return self.__operate(other, add)
+        return self.__operator(other, add)
 
     def subt(self, other):
-        return self.__operate(other, sub)
+        return self.__operator(other, sub)
 
     def transpose(self):
-        return tuple(zip(*list(self.data)))
+        return Matrix(
+            self.cols,
+            self.rows,
+            lambda: tuple(sorted([(col, row, val) for row, col, val in self.data], key=lambda tup: tup[0]))
+        )
 
     def __repr__(self):
-        return '\n'.join([f'{row}' for row in self.data])
+        return '\n'.join([f'{row}' for row in self.__matrix()])
 
 
 def gen():
@@ -71,14 +79,15 @@ print('----------------------------------------')
 
 m = Matrix(2, 3, gen)
 print(m)
-
-m = m.mult(m.transpose())
-print(m)
+print(m.transpose())
+print(m.sum(m))
+print(m.subt(m))
+print(m.mult_scalar(5))
+print(m.mult(m.transpose()))
 
 m = Matrix(3, 3, lambda: ())
-m_gen = Matrix(3, 3, gen)
-m_gen_ex = Matrix(3, 3, gen_ex(3, 3))
+print(m)
 
-print(m_gen_ex.sum(m_gen.data))
-print(m_gen_ex.subt(m_gen.data))
-print(m_gen_ex.mult_scalar(5))
+m = Matrix(3, 3, gen_ex(3, 3))
+print(m)
+print(m.mult(m.transpose()).mult_scalar(10))
